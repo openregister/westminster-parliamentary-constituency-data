8bf84ed5eccbbcec37559abe430f4a81c2c6116b
Duncan Garmonsway <nacnudus@gmail.com>
2017-10-26 13:17:40 +0100

    Fix legislation scrape for Richmond (Yorks)

--- R/legislation.R
+++ R/legislation.R
@@ -13,17 +13,20 @@ x2007 <- read_html("http://www.legislation.gov.uk/uksi/2007/1681/schedule/made")
 x2007 %>%
   html_table(header = FALSE) %>%
   bind_rows() %>%
-  select(1) %>%
-  filter(!str_detect(tolower(X1), "^in "),
-         !str_detect(tolower(X1), "name and designation"),
-         X1 != toupper(X1)) %>%
-  mutate(X1 = str_replace_all(X1, "\\r\\n", " "),
-         X1 = str_replace_all(X1, "\\r", " "),
-         X1 = str_replace_all(X1, " \\(.*", ""),
+  rename(name = X1) %>%
+  select(name) %>%
+  filter(!str_detect(tolower(name), "^in "),
+         !str_detect(tolower(name), "name and designation"),
+         !str_detect(tolower(name), "the london borough"),
+         name != toupper(name)) %>%
+  mutate(name = str_replace_all(name, "\\r\\n", " "),
+         name = str_replace_all(name, "\\r", " "),
+         name = str_replace_all(name, " \\(county.*", ""),
+         name = str_replace_all(name, " \\(borough.*", ""),
          `westminster-parliamentary-constituency` = row_number() + 100,
          `start-date` = "2010-04-12",
          `end-date` = NA) %>%
-  rename(name = X1) %>%
+  arrange(name) %>%
   select(`westminster-parliamentary-constituency`, everything()) %>%
   write_tsv(here("lists", "legislation", "constituencies-2007.tsv"), na = "")
 
@@ -32,17 +35,19 @@ x2009 <- read_html("http://www.legislation.gov.uk/uksi/2009/698/schedule/made")
 x2009 %>%
   html_table(header = FALSE) %>%
   bind_rows() %>%
-  select(1) %>%
-  filter(!str_detect(tolower(X1), "^in "),
-         !str_detect(tolower(X1), "name and designation"),
-         X1 != toupper(X1)) %>%
-  mutate(X1 = str_replace_all(X1, "\\r\\n", " "),
-         X1 = str_replace_all(X1, "\\r", " "),
-         X1 = str_replace_all(X1, " \\(.*", ""),
+  rename(name = X1) %>%
+  select(name) %>%
+  filter(!str_detect(tolower(name), "^in "),
+         !str_detect(tolower(name), "name and designation"),
+         !str_detect(tolower(name), "the london borough"),
+         name != toupper(name)) %>%
+  mutate(name = str_replace_all(name, "\\r\\n", " "),
+         name = str_replace_all(name, "\\r", " "),
+         name = str_replace_all(name, " \\(county.*", ""),
+         name = str_replace_all(name, " \\(borough.*", ""),
          `westminster-parliamentary-constituency` = NA, # complete manually
          `start-date` = "2010-04-12",
          `end-date` = NA) %>%
-  rename(name = X1) %>%
+  arrange(name) %>%
   select(`westminster-parliamentary-constituency`, everything()) %>%
   write_tsv(here("lists", "legislation", "constituencies-2009.tsv"), na = "")
-
